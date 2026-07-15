32bb300ca7abb5ac746bc3916ffa8be2080047e5
Matt White <matt@matthewjwhite.co.uk>
2026-07-15 21:01:38 +0100

    try and do new provider

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -1,1 +1,18 @@
-
+resource "unifi_network" "lan_network" {
+  name = "Management"
+  purpose = "corporate"
+  subnet = "10.140.254.1/24"
+  vlan = 999
+  dhcp_relay = {
+    enabled = true
+    servers = ["192.168.178.14"]
+  }
+  dhcp_guarding = {
+    enabled = true
+    servers = ["192.168.178.14"]
+  }
+  ipv6_interface_type = "static"
+  ipv6_static_subnet = "2a02:8011:ee07:999::/64"
+  ipv6_client_address_assignment = "slaac"
+  ipv6_ra = true
+}
