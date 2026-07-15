1e6de3f182d51c85eb48d26e1ecd63a10c9e9394
Matt White <matt@matthewjwhite.co.uk>
2026-07-15 17:41:59 +0100

    remove v6 to test

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -5,7 +5,7 @@ resource "unifi_network" "lan_network" {
   vlan_id             = 999
   dhcp_enabled        = false
   enabled             = true
-  ipv6_interface_type = "pd"
-  ipv6_pd_interface   = "wan"
-  ipv6_ra_enable      = "true"
+  #ipv6_interface_type = "pd"
+  #ipv6_pd_interface   = "wan"
+  #ipv6_ra_enable      = "true"
 }
