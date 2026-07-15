00a869611a5adc0b4ae1d639586a83847a17bca7
Matt White <matt@matthewjwhite.co.uk>
2026-07-15 17:29:32 +0100

    test create a network

---
+++ terraform/networks.tf
@@ -0,0 +1,12 @@
+resource "unifi_network" "lan_network" {
+  name                = "Management"
+  purpose             = "corporate"
+  subnet              = "10.140.254.0/24"
+  vlan_id             = 999
+  dhcp_enabled        = false
+  enabled             = true
+  ipv6_interface_type = "pd"
+  ipv6_pd_interface   = "wan"
+  ipv6_pd_prefix_id   = "999"
+  ipv6_ra_enable     = "true"
+}
