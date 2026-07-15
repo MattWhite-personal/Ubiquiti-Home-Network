c570e41f28cff9c3f05ecf31c1d92635a9176685
Matt White <matt@matthewjwhite.co.uk>
2026-07-15 17:46:12 +0100

    fix fmt

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -1,10 +1,10 @@
 resource "unifi_network" "lan_network" {
-  name                = "Management"
-  purpose             = "corporate"
-  subnet              = "10.140.254.0/24"
-  vlan_id             = 999
-  dhcp_enabled        = false
-  enabled             = true
+  name         = "Management"
+  purpose      = "corporate"
+  subnet       = "10.140.254.0/24"
+  vlan_id      = 999
+  dhcp_enabled = false
+  enabled      = true
   #ipv6_interface_type = "pd"
   #ipv6_pd_interface   = "wan"
   #ipv6_ra_enable      = "true"
