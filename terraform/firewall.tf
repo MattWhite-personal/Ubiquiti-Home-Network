8549b06e915b508c7121846e47603a71df29eb3c
Matt White <matt@matthewjwhite.co.uk>
2026-07-25 21:25:51 +0100

    try and avoid default zone errors

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -1,6 +1,7 @@
-#resource "unifi_firewall_zone" "hotspot" {
-#  name = "Hotspot"
-#}
+resource "unifi_firewall_zone" "hotspot" {
+  name         = "Hotspot"
+  default_zone = true
+}
 
 resource "unifi_firewall_zone" "mjw-test" {
   name = "MJW Test"
