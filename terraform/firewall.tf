ba95ed20a90a47631dce48a9d65f925702bf66de
Matt White <matt@matthewjwhite.co.uk>
2026-07-25 21:03:53 +0100

    add dummy zone

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -1,3 +1,7 @@
-resource "unifi_firewall_zone" "hotspot" {
-  name = "Hotspot"
+#resource "unifi_firewall_zone" "hotspot" {
+#  name = "Hotspot"
+#}
+
+resource "unifi_firewall_zone" "mjw-test" {
+    name = "MJW Test"
 }
