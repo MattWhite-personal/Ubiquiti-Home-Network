498e9f01ce39c55969b418a92d0ea95958890e7a
Matt White <matt@matthewjwhite.co.uk>
2026-07-25 19:00:09 +0100

    add values

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,7 +2,9 @@ data "unifi_network" "lan_network" {
   name = "Default"
 }
 data "unifi_ap_group" "default" {
+  name = "Default"
 }
 
 data "unifi_client_qos_rate" "default" {
+  name = "Default"
 }
