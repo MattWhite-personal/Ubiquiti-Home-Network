6a783fae3a6da403fc4675369abc8633ed8ac215
Matt White <matt@matthewjwhite.co.uk>
2026-07-25 22:25:58 +0100

    add guest network following import

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -1,6 +1,9 @@
 resource "unifi_firewall_zone" "hotspot" {
   name         = "Hotspot"
   default_zone = true
+  network_ids = [
+    unifi_network.network["guest"].id
+  ]
 }
 
 resource "unifi_firewall_zone" "mjw-test" {
