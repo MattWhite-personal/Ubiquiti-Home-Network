a68ffe45aeb6e8eef5bbebf1b9835fad33fb30d4
Matt White <matt@matthewjwhite.co.uk>
2026-07-31 21:56:46 +0100

    fix tf fmt

--- terraform/locals_wifi.tf
+++ terraform/locals_wifi.tf
@@ -37,9 +37,9 @@ locals {
     personal     = { network_slug = "personal", cred_var = "radius_personal" }
   }
   wifi_secrets = {
-    wifi_guest_psk = var.wifi_guest_psk
-    wifi_iot_psk   = var.wifi_iot_psk
-    radius_matt    = var.radius_matt
+    wifi_guest_psk  = var.wifi_guest_psk
+    wifi_iot_psk    = var.wifi_iot_psk
+    radius_matt     = var.radius_matt
     radius_jen      = var.radius_jen
     radius_personal = var.radius_personal
   }
