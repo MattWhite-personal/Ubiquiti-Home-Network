f075f9d66a952394d95518e2c7f8f770aacc09a9
Matt White <matt@matthewjwhite.co.uk>
2026-07-31 21:25:30 +0100

    use net facts for vlan id

--- terraform/radius.tf
+++ terraform/radius.tf
@@ -3,7 +3,7 @@ resource "unifi_radius_user" "user" {
 
   name               = each.key
   password           = local.wifi_secrets[each.value.cred_var]
-  vlan               = unifi_network.network[each.value.network_slug].vlan_id
+  vlan               = local.network_facts[each.value.network_slug].vlan_id
   tunnel_config_type = "802.1x"
   tunnel_type        = 13 # VLAN
   tunnel_medium_type = 6  # IEEE-802
