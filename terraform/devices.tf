8f8b32d71f7f91ec2f15e7eb60bbd2d90ee516ea
Matt White <matt@matthewjwhite.co.uk>
2026-08-08 21:59:23 +0100

    keep mgmt on dhcp for now

--- terraform/devices.tf
+++ terraform/devices.tf
@@ -9,13 +9,13 @@ resource "unifi_device" "device" {
   mac             = each.value.mac
   name            = each.value.name
   mgmt_network_id = each.value.mgmt_network != null ? local.network_id[each.value.mgmt_network] : null
-  config_network = each.value.mgmt_network != null ? {
-    type    = "static"
-    ip      = cidrhost(local.network_facts[each.value.mgmt_network].ipv4_subnet, each.value.mgmt_ip)
-    netmask = cidrnetmask(local.network_facts[each.value.mgmt_network].ipv4_subnet)
-    gateway = split("/", local.network_facts[each.value.mgmt_network].ipv4_gateway)[0]
-    dns1    = var.pihole_ipv4
-  } : null
+  #config_network = each.value.mgmt_network != null ? {
+  #  type    = "static"
+  #  ip      = cidrhost(local.network_facts[each.value.mgmt_network].ipv4_subnet, each.value.mgmt_ip)
+  #  netmask = cidrnetmask(local.network_facts[each.value.mgmt_network].ipv4_subnet)
+  #  gateway = split("/", local.network_facts[each.value.mgmt_network].ipv4_gateway)[0]
+  #  dns1    = var.pihole_ipv4
+  #} : null
   dynamic "port_override" {
     for_each = each.value.ports
     content {
