641533cf14c3cab71f3295154c444e7020dd56f1
Matt White <matt@matthewjwhite.co.uk>
2026-08-09 15:56:40 +0100

    enable stp on all port profiles

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -49,6 +49,7 @@ resource "unifi_port_profile" "port_profile" {
   full_duplex        = true
   dot1x_ctrl         = "auto"
   setting_preference = "manual"
+  stp_port_mode      = true
 
   forward = (
     each.value.state == "disabled" ? "disabled" :
