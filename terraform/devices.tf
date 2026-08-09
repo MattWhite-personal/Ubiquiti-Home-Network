8ebac59712df8284d9af74180359d417d8ac18c2
Matt White <matt@matthewjwhite.co.uk>
2026-08-09 13:41:26 +0100

    dns suffix object typo

--- terraform/devices.tf
+++ terraform/devices.tf
@@ -12,12 +12,12 @@ resource "unifi_device" "device" {
   stp_priority    = each.value.stp_priority
   stp_version     = each.value.stp_version
   config_network = each.value.mgmt_ip != null ? {
-    type       = "static"
-    ip         = "10.140.254.${each.value.mgmt_ip}"
-    netmask    = "255.255.255.0"
-    gateway    = "10.140.254.1"
-    dns1       = "192.168.178.11"
-    dns_suffix = "gilbert.road"
+    type      = "static"
+    ip        = "10.140.254.${each.value.mgmt_ip}"
+    netmask   = "255.255.255.0"
+    gateway   = "10.140.254.1"
+    dns1      = "192.168.178.11"
+    dnssuffix = "gilbert.road"
   } : null
   dynamic "port_override" {
     for_each = each.value.ports
