42c8f289cc0cb8765c512ed49ce774dd355266f9
Matt White <matt@matthewjwhite.co.uk>
2026-08-09 14:46:56 +0100

    second ai fix

--- terraform/devices.tf
+++ terraform/devices.tf
@@ -24,7 +24,7 @@ resource "unifi_device" "device" {
     content {
       index           = port_override.key
       name            = port_override.value.name
-      port_profile_id = local.port_profiles[port_override.value.port_profile]
+      port_profile_id = unifi_port_profile.port_profile[port_override.value.port_profile].id
     }
   }
 }
